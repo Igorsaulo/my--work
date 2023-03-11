96f3177aff9b8d36f65c51bc541289a13f5791f4
igorsaulo <igorsaulo01@gmail.com>
2023-03-11 01:06:19 -0300

    update

---
+++ pages/api/auth/index.js
@@ -0,0 +1,38 @@
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const prisma = new PrismaClient();
+
+export default async function Users (req,res){
+    await prisma.$connect()
+    const { method } =req
+    switch (method){
+        case 'POST':
+        try {
+            const {email,password} = req.body
+
+            const user = await prisma.Users.findUnique({
+                where:{
+                    email:email
+                }
+            })
+            const comparar = bcrypt.compare(password,user.password)
+            if(!comparar){
+                return res.status(422).json({msg:'Senha invalida'})
+            }
+            const secret = process.env.SECRET
+            const token = jwt.sign(
+                {
+                    id:user._id,
+                    username:user.username,
+                },
+                secret,
+                {expiresIn: "1d"}
+                )
+            
+            res.status(200).json({msg:'Autenticação realizada com sucesso',user:user.username,token:token})
+        }catch(error){
+            console.log(error)
+        }
+    }
+};s
---
+++ pages/api/register/index.js
@@ -0,0 +1,65 @@
+const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcrypt')
+
+const prisma = new PrismaClient()
+export default async function Users (req,res){
+    await prisma.$connect()
+    const { method } =req
+    switch (method){
+        case 'GET':
+        try {
+            const users = await prisma.Users.findMany()
+            return res.status(200).json(users)
+        } catch (erro) {
+            return res.status(400).json(erro)
+        }
+        break
+        case 'POST':
+            try {
+                let {email,password,username} = await req.body
+                password = bcrypt.hashSync(password,12)
+                const user = await prisma.Users.create({data:{
+                    email:email,
+                    password:password,
+                    username:username
+                },})
+                console.log(user)
+                return res.status(201).json({ success: true, data: user })
+            } catch (error) {
+                return res.status(400).json({ success: false })
+            }
+            break
+            case 'PATCH':
+                try {
+                    const {email,novoEmail} = req.body
+                    console.log(req.body)
+                    console.log(email)
+                    console.log(novoEmail)
+                    const att = await prisma.Users.update({
+                        where:{
+                            email:email,
+                        },
+                        data:{
+                            email:novoEmail,
+                        },
+                    })
+                    console.log(att)
+                    res.status(201).json({ success: true, data: "atualizado" })
+                } catch (erro) {
+                    res.status(400).json({ success: false,error:erro })
+                }
+            break
+            case 'DELETE':
+                try{
+                    const {email} = await req.body
+                    await prisma.Users.delete({
+                        where:{
+                            email:email,
+                        },
+                    },)
+                    res.status(201).json({success:true})
+                }catch(error){
+                    res.status(400).json({success:false})
+                }
+    }
+}
